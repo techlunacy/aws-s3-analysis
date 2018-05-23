56309616a451013b9c39ad9bb28dc1e31d8411f8
Graeme W <graeme@techlunacy.com>
2018-05-23 23:42:39 +0800

    format the storage and dates

--- s3.py
+++ s3.py
@@ -2,27 +2,34 @@
 
 import sys
 import datetime
+from collections import defaultdict
 import boto3
 
 def list():
     s3 = boto3.resource('s3')
-
+    buckets = []
     for bucket in s3.buckets.all():
+        region = get_region(bucket.name)
         objects = get_objects(bucket.name)
         bucket_map = {'name': bucket.name,
-                    'creation_date': bucket.creation_date, 
-                    'object_size': 0, 
+                    'bucket_creation_date': bucket.creation_date, 
+                    'storage': defaultdict(int), 
                     'count': 0,
-                    'last_modified_date': datetime.datetime(1,1,1,tzinfo=datetime.timezone.utc)} 
-     
+                    'last_modified_date': datetime.datetime(1,1,1,tzinfo=datetime.timezone.utc),
+                    'region': region,} 
+    
         for o in objects:
             bucket_map['count'] = bucket_map['count'] + 1
             # print(o)
-            bucket_map['object_size'] += o['Size']
+            bucket_map['storage'][o['StorageClass']] += o['Size']
             if bucket_map['last_modified_date'] < o['LastModified']:
                 bucket_map['last_modified_date'] = o['LastModified']
-        print("{0}".format(bucket_map))
+        buckets.append(bucket_map)
+    return buckets
 
+def get_region(bucket_name):
+    s3 = boto3.resource('s3')
+    return s3.meta.client.get_bucket_location(Bucket=bucket_name)["LocationConstraint"]
 
 
 def get_objects(bucket_name, prefix=''):
@@ -36,6 +43,25 @@ def get_objects(bucket_name, prefix=''):
         for o in page['Contents']:
             yield o
 
+def divisor_map(storage_display_format):
+    try:
+        return {'b':1,
+                'kb': 1024,
+                'mb': 1024 * 1024,
+                'gb': 1024 * 1024 * 1024,}[storage_display_format]
+    except KeyError:
+        raise ValueError("{0} is not a valid storage_display_format".format(storage_display_format))
+
+
+def format_bucket(buckets, storage_display_format='gb'):
+    
+    division = divisor_map(storage_display_format.lower())
+    bucket['bucket_creation_date'] = bucket['bucket_creation_date'].isoformat()
+    bucket['last_modified_date'] = bucket['last_modified_date'].isoformat() 
+    for key, value in bucket['storage'].items():
+        bucket['storage'][key] = "{0} {1}".format(round(value/division, 2),storage_display_format)
+    return bucket
+
 def configure():
     print('configure')
 
@@ -43,6 +69,13 @@ if __name__ == "__main__":
     # execute only if run as a script
     command = sys.argv[1]
     if command == 'list':
-        list()
+        buckets = list()
+        for bucket in buckets:
+            if len(sys.argv)>2:
+                storage_display_format = sys.argv[2]
+                bucket  = format_bucket(bucket, storage_display_format)
+            else:
+                bucket  = format_bucket(bucket)
+        print("{0}".format(buckets))
     else:
         configure()
