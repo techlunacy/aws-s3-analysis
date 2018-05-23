2d71c71edfefbd470c065b74bbfe6a7d69e3508a
Graeme W <graeme@techlunacy.com>
2018-05-23 21:50:07 +0800

    refactor to use a map

--- s3.py
+++ s3.py
@@ -5,27 +5,32 @@ import boto3
 
 def list():
     s3 = boto3.resource('s3')
-    for bucket in s3.buckets.all():
-        get_details(bucket)
 
-def get_details(bucket, prefix=''):
+    for bucket in s3.buckets.all():
+        objects = get_objects(bucket.name)
+        bucket_map = {'name': bucket.name,
+                    'creation_date': bucket.creation_date, 
+                    'object_size': 0, 
+                    'size': len(objects),}       
+        for o in objects:
+            bucket_map['object_size'] += o['Size'] 
+        
+        print("{0}".format(bucket_map))
+
+
+
+def get_objects(bucket_name, prefix=''):
     
     client = boto3.client('s3')
     # Create a reusable Paginator
     paginator = client.get_paginator('list_objects_v2')
-    operation_parameters = {'Bucket': bucket.name,
+    operation_parameters = {'Bucket': bucket_name,
                             'Prefix': prefix}
     page_iterator = paginator.paginate(**operation_parameters)
     objects = []
     for page in page_iterator:
         objects += page['Contents']
-    object_size = 0
-    for o in objects:
-        # print(o)
-        object_size += o['Size'] 
-    
-    print("{0},{1},{2} ".format(bucket.name, len(objects), object_size))
-
+    return objects
 def configure():
     print('configure')
 
