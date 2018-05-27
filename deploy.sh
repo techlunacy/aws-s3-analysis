f27be492c37c7012db6de9dde4c3a15f510491f4
Graeme W <graeme@techlunacy.com>
2018-05-27 18:43:58 +0800

    git st

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ terraform apply -auto-approve
 
 cd ../ansible
 # pull the instance information from Terraform, and run the Ansible playbook against it to configure
-TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision.yml
+TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision.yml -vvv
 
 echo "Success!"
 
