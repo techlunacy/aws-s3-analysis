5fa0bc0b9771806ce42fddacdc136fb3d3298d14
Graeme W <graeme@techlunacy.com>
2018-05-27 14:02:31 +0800

    format the sizes

--- s3.py
+++ s3.py
@@ -29,11 +29,33 @@ class S3bucket:
                             self.last_modified_date.isoformat())
 
     def get_objects(self, prefix):
-        return list(self.bucket.objects.filter(Prefix=prefix))
+        return list(self.bucket.objects.filter(Prefix=prefix.replace("*", "")))
 
     def get_region(self):
         return self.bucket.meta.client.get_bucket_location(Bucket=self.name)["LocationConstraint"]
 
+    def format(self, bucket_size_format):
+        storage_string = ""
+        for k,v in self.storage.items():
+            value = round(v/self.divisor_map(bucket_size_format), 2)
+            storage_string+= "{},{}{},".format(k,value, bucket_size_format)
+        return "{},{},{},{},{},{}".format(self.name,
+                            self.region,
+                            self.count,
+                            self.creation_date.isoformat(), 
+                            self.last_modified_date.isoformat()
+                            ,storage_string)
+
+    @staticmethod
+    def divisor_map(storage_display_format):
+        try:
+            return {'b':1,
+                    'kb': 1024,
+                    'mb': 1024 * 1024,
+                    'gb': 1024 * 1024 * 1024,}[storage_display_format]
+        except KeyError:
+            raise ValueError("{0} is not a valid storage_display_format".format(storage_display_format))
+    
     @staticmethod
     def connection():
         return boto3.resource('s3')
@@ -42,6 +64,10 @@ class S3bucket:
     def get_all_buckets():
         for b in S3bucket.connection().buckets.all():        
             yield S3bucket(b)
+    
+    @staticmethod
+    def get_bucket(bucket_name, prefix):
+        return S3bucket(S3bucket.connection().Bucket(bucket_name), prefix)            
 
 
 # def divisor_map(storage_display_format):
@@ -65,20 +91,19 @@ class S3bucket:
 
 if __name__ == "__main__":
     command = sys.argv[1]
+    list_buckets = []
+    bucket_size_format = "gb"
     if command == 'list':
         for b in S3bucket.get_all_buckets():
-            print(b)
-    # # execute only if run as a script
-    #     buckets = list()
-    #     for bucket in buckets:
-    #         if len(sys.argv)>2 and sys.argv[2] != 'group':
-    #             storage_display_format = sys.argv[2]
-    #             bucket  = format_bucket(bucket, storage_display_format)
-    #         else:
-    #             bucket  = format_bucket(bucket)
-            
-    #     if 'group' in sys.argv:
-    #         buckets = group_by_region(buckets)
-    #     print("{0}".format(buckets))
-    # else:
-    #     configure()
+            list_buckets.append(b)
+    elif command == 'get':
+        path = sys.argv[2]
+        split_path = path.split('/', 3)
+        list_buckets.append(S3bucket.get_bucket(bucket_name=split_path[2], prefix=split_path[3]))
+
+    if  '--format' in sys.argv:
+        index_of_format = sys.argv.index('--format') + 1
+        bucket_size_format = sys.argv[index_of_format]
+
+    for b in list_buckets:
+        print(b.format(bucket_size_format))
